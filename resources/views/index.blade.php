1a2d7f4a44194c230b7e81f9f679a29bf7fa4715
Charles <bernaldezsay@gmail.com>
2020-09-12 21:58:04 +0800

    submit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,10 +10,25 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
 
     {{-- CSS --}}
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
 
     {{-- JQuery --}}
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
+    <style>
+        #btn{
+            background-color:rgb(170, 68, 68);
+            border: none;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+        }
+    </style>
 
 </head>
 <body>
--- resources/views/paint-job.blade.php
+++ resources/views/paint-job.blade.php
@@ -10,10 +10,17 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
 
     {{-- CSS --}}
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
 
     {{-- JQuery --}}
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
 
 </head>
 <body>
@@ -28,7 +35,7 @@
         </section>
 
         <section class="lower-section">
-            <table>
+            <table style="width: 100%">
                 <thead>
                     <tr>
                         <th>Plate No</th>
